8a1b569f98150b829c9a3fa4fb7d5e7f4488a7e5
i-k-26 <kyo_ko_ku@yahoo.co.jp>
2019-10-04 20:11:05 +0900

    SVGを使用したHTMLベースの検索機能

---
+++ areamap/areamap.js
@@ -0,0 +1,106 @@
+$(function () {
+    $('#yoyogi-area,#yoyogi').hover(function () {
+        $('#yoyogi-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#yoyogi-area').css('fill', '#e9e6ba');
+    });
+});
+
+
+
+$(function () {
+    $('#oyamacho-area,.oyamacho').hover(function () {
+        $('#oyamacho-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#oyamacho-area').css('fill', '#e9e6ba');
+    });
+});
+
+$(function () {
+    $('#tomigaya-area,#tomigaya').hover(function () {
+        $('#tomigaya-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#tomigaya-area').css('fill', '#e9e6ba');
+    });
+});
+
+$(function () {
+    $('#shibuya-area,#shibuya').hover(function () {
+        $('#shibuya-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#shibuya-area').css('fill', '#e9e6ba');
+    });
+});
+
+$(function () {
+    $('#shoto-area,#shoto').hover(function () {
+        $('#shoto-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#shoto-area').css('fill', '#e9e6ba');
+    });
+});
+
+$(function () {
+    $('#kamiyamacho-area,#kamiyamacho').hover(function () {
+        $('#kamiyamacho-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#kamiyamacho-area').css('fill', '#e9e6ba');
+    });
+});
+
+$(function () {
+    $('#ebisu-area,.ebisu').hover(function () {
+        $('#ebisu-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#ebisu-area').css('fill', '#e9e6ba');
+    });
+});
+
+$(function () {
+    $('#meguro-area,#meguro').hover(function () {
+        $('#meguro-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#meguro-area').css('fill', '#c6dde7');
+    });
+});
+
+
+$(function () {
+    $('#akasaka-area,#akasaka').hover(function () {
+        $('#akasaka-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#akasaka-area').css('fill', '#d2dfba');
+    });
+});
+
+$(function () {
+    $('#aoyama-area,#aoyama').hover(function () {
+        $('#aoyama-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#aoyama-area').css('fill', '#d2dfba');
+    });
+});
+
+$(function () {
+    $('#roppongi-area,#roppongi').hover(function () {
+        $('#roppongi-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#roppongi-area').css('fill', '#d2dfba');
+    });
+});
+
+$(function () {
+    $('#azabu-area,#azabu').hover(function () {
+        $('#azabu-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#azabu-area').css('fill', '#d2dfba');
+    });
+});
+
+$(function () {
+    $('#shirokane-area,#shirokane').hover(function () {
+        $('#shirokane-area').css('fill', '#ff8c00');
+    }, function () {
+        $('#shirokane-area').css('fill', '#d2dfba');
+    });
+});
